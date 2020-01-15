7dcebf3d25ec8a8bb4a11b4b0f8d579560d9c216
Charles Moulliard <cmoulliard@redhat.com>
2020-01-15 14:52:46 +0100

    chore: WIP. Refactor the project to use the component class to pass the parameters and collect the information

--- src/main/java/me/snowdrop/google/Component.java
+++ src/main/java/me/snowdrop/google/Component.java
@@ -7,7 +7,52 @@ import java.net.URL;
 public class Component {
 
     public Model model;
+    public Model parentModel;
     public URL repoURL;
+    public String groupId;
+    public String artifactId;
+    public String version;
+    public String toSearch;
+
+    public Model getParentModel() {
+        return parentModel;
+    }
+
+    public void setParentModel(Model parentModel) {
+        this.parentModel = parentModel;
+    }
+
+    public String getToSearch() {
+        return toSearch;
+    }
+
+    public void setToSearch(String toSearch) {
+        this.toSearch = toSearch;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
 
     public URL getRepoURL() {
         return repoURL;
--- src/main/java/me/snowdrop/google/GSheetApplication.java
+++ src/main/java/me/snowdrop/google/GSheetApplication.java
@@ -72,7 +72,7 @@ public class GSheetApplication {
         }
         // Parse the configuration file to setup the parameters
         ObjectMapper mapper = new ObjectMapper();
-        configuration = mapper.readValue(configURL,Configuration.class);
+        configuration = mapper.readValue(configURL, Configuration.class);
     }
 
     static Sheets createService() throws GeneralSecurityException, IOException {
@@ -98,26 +98,24 @@ public class GSheetApplication {
             for (int i = 1; i < values.size(); i++) {
                 List<Object> row = values.get(i);
                 try {
-                    String componentToSearch = (String) row.get(3);
-                    if (!componentToSearch.contentEquals("??")) {
-                        System.out.printf("Component : %s\n", componentToSearch);
+                    Component component = new Component();
+                    component.toSearch = (String) row.get(3);
+                    if (!component.toSearch.contentEquals("??")) {
+                        System.out.printf("Component : %s\n", component.toSearch);
 
                         // Fetch POM file using GAV defined within the G Sheet
                         // and maven repository upstream
-                        Component component = parseMavenPOM(configuration.mavenCentralRepo, (String) row.get(0), (String) row.get(1), (String) row.get(2));
+                        component = parseMavenPOM(configuration.mavenCentralRepo, (String) row.get(0), (String) row.get(1), (String) row.get(2));
                         System.out.printf("Maven URL : %s\n", component.repoURL);
 
                         // Check if the dependency contains the component to search and get version
-                        String componentVersion = getComponentVersion(component.model, componentToSearch);
-                        System.out.printf("Version : %s\n", componentVersion);
+                        component = getComponentVersion(component);
+                        System.out.printf("Version : %s\n", component.version);
 
-                        // Update the cell of the Component Version (upstream)
-                        String cellPosition = configuration.getCellUpstreamVersion() + (i + 1);
-                        updateCells(service, cellPosition, "RAW", componentVersion);
-
-                        // Update the cell of the Component URL (upstream)
-                        String cellURLPosition = configuration.getCellUpstreamURL() + (i + 1);
-                        String hyperlink = "=HYPERLINK(\"" + component.getRepoURL().toString() + "\",\"" + componentVersion + "\")";
+                        // Update the cell of the Component URL upstream using as text, the version and
+                        // link, the url of the maven repo to access the pom file
+                        String cellURLPosition = configuration.getCellUpstreamVersion() + (i + 1);
+                        String hyperlink = "=HYPERLINK(\"" + component.getRepoURL().toString() + "\",\"" + component.getVersion() + "\")";
                         updateCells(service, cellURLPosition, "USER_ENTERED", hyperlink);
 
                         // Do the job for Downstream Repository
@@ -194,85 +192,91 @@ public class GSheetApplication {
         }
     }
 
-    static String getComponentVersion(Model model, String componentToSearch) throws IOException, XmlPullParserException {
-        List<Dependency> dependencies = model.getDependencies();
+    static Component getComponentVersion(Component component) throws IOException, XmlPullParserException {
+        List<Dependency> dependencies = component.getModel().getDependencies();
         String result;
 
-        // Test if we have dependencies, otherwise use the dependencies delcared within the dependencyManagement section
+        // Test if we have dependencies within the pom, otherwise use the dependencies declared within the dependencyManagement section
+        // of the Parent Pom
         if (dependencies.size() > 0) {
             for (Dependency dep : dependencies) {
-                result = searchVersion(model, dep, componentToSearch);
-                if (result != "") {
-                    return result;
+                component = searchVersion(component, dep);
+                if (component.getVersion() != "") {
+                    return component;
                 }
             }
         } else {
-            for (Dependency dep : model.getDependencyManagement().getDependencies()) {
-                result = searchVersion(model, dep, componentToSearch);
-                if (result != "") {
-                    return result;
+            for (Dependency dep : component.getParentModel().getDependencyManagement().getDependencies()) {
+                component = searchVersion(component, dep);
+                if (component.getVersion() != "") {
+                    return component;
                 }
             }
         }
-        return "";
+        return component;
     }
 
-    static String searchVersion(Model model, Dependency dependency, String componentToSearch) throws IOException, XmlPullParserException {
-        if (dependency.getArtifactId().contains(componentToSearch)) {
+    static Component searchVersion(Component component, Dependency dependency) throws IOException, XmlPullParserException {
+        if (dependency.getArtifactId().contains(component.toSearch)) {
             // If the version is null, then we will search the version of the component
             // using the dependencies defined within the parent pom
             if (dependency.getVersion() == null) {
-                Parent parent = model.getParent();
-                Component component = parseMavenPOM(configuration.mavenCentralRepo, parent.getGroupId().replaceAll("\\.", "/"), parent.getArtifactId(), parent.getVersion());
-                return getComponentVersion(component.model, componentToSearch);
+                Parent parent = component.getModel().getParent();
+                component = parseMavenPOM(configuration.mavenCentralRepo, parent.getGroupId().replaceAll("\\.", "/"), parent.getArtifactId(), parent.getVersion()));
+                return getComponentVersion(component);
             }
 
             if (dependency.getVersion().startsWith("${project.version")) {
                 // Check if there is a version defined for the project, otherwise pickup the version of the parent
-                if (model.getVersion() == null) {
-                    return model.getParent().getVersion();
+                if (component.getModel().getVersion() == null) {
+                    component.setVersion(component.getModel().getParent().getVersion());
+                    return component;
                 } else {
-                    return model.getVersion();
+                    component.setVersion(component.getModel().getVersion());
+                    return component;
                 }
             }
 
             // We will check if we have a version or ${}"
             if (dependency.getVersion().startsWith("${")) {
-                Properties props = model.getProperties();
+                Properties props = component.getModel().getProperties();
                 Set<Map.Entry<Object, Object>> entries = props.entrySet();
                 for (Map.Entry<Object, Object> entry : entries) {
                     String key = (String) entry.getKey();
-                    if (key.contains(componentToSearch)) {
+                    if (key.contains(component.toSearch)) {
                         String val = (String) entry.getValue();
                         // If the key is not a version such as a string, message, then we continue
                         if (val.contains(" ")) {
                             continue;
                         }
-                        return val;
+                        component.setVersion(val);
+                        return component;
                     }
                 }
                 // If there are no properties, then we will check if the parent contains it
-                Parent parent = model.getParent();
-                Component component = parseMavenPOM(configuration.getMavenCentralRepo(), parent.getGroupId().replaceAll("\\.", "/"), parent.getArtifactId(), parent.getVersion());
+                Parent parent = component.getModel().getParent();
+                component.setParentModel(parseMavenPOM(configuration.getMavenCentralRepo(), parent.getGroupId().replaceAll("\\.", "/"), parent.getArtifactId(), parent.getVersion()));
                 // TODO : To be improved
                 props = component.getModel().getProperties();
                 entries = props.entrySet();
                 for (Map.Entry<Object, Object> entry : entries) {
                     String key = (String) entry.getKey();
-                    if (key.contains(componentToSearch)) {
+                    if (key.contains(component.toSearch)) {
                         String val = (String) entry.getValue();
                         // If the key is not a version such as a string, message, then we continue
                         if (val.contains(" ")) {
                             continue;
                         }
-                        return val;
+                        component.setVersion(val);
+                        return component;
                     }
                 }
 
             }
-            return dependency.getVersion();
+            component.setVersion(dependency.getVersion());
+            return component;
         }
-        return "";
+        return component;
     }
 
     static Component parseMavenPOM(String mavenRepo, String groupID, String artifactID, String version) throws IOException, XmlPullParserException {
